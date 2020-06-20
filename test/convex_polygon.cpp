7230b67f5f8f02e09a3a850e2e561cc4c22503c9
Ghostkeeper <rubend@tutanota.com>
2020-06-20 17:50:46 +0200

    Add test for the copy constructor

--- test/convex_polygon.cpp
+++ test/convex_polygon.cpp
@@ -27,4 +27,14 @@ TEST(ConvexPolygon, Construct) {
 	EXPECT_EQ(poly.get_vertices(), vertices) << "The vertices in the convex polygon must be the same as the input.";
 }
 
+/*!
+ * Tests the copy constructor.
+ */
+TEST(ConvexPolygon, ConstructCopy) {
+	std::vector<Point2> vertices = {Point2(10.1, 20.2), Point2(-40.4, 30.3), Point2(0, 20.2)};
+	const ConvexPolygon original(vertices);
+	const ConvexPolygon copy(original);
+	EXPECT_EQ(original.get_vertices(), copy.get_vertices()) << "The copy must be an exact copy.";
+}
+
 }
