ad9132b4db410a0041dc61f557f21b43d6ce6798
Ghostkeeper <rubend@tutanota.com>
2020-06-21 01:57:31 +0200

    Create a fixture with some re-usable vertex lists
    That'll make it easier to write these tests.

--- test/convex_polygon.cpp
+++ test/convex_polygon.cpp
@@ -5,9 +5,49 @@
 
 #include <gtest/gtest.h> //To run the test.
 #include <vector> //To store vertices of test polygons.
-#include <convex_polygon.hpp> //The unit under test.
+
+#include "convex_polygon.hpp" //The unit under test.
 
 namespace convack {
+
+/*!
+ * Fixture with some base test cases to test the ConvexPolygon class with.
+ */
+class ConvexPolygonFixture : public testing::Test {
+public:
+	/*!
+	 * A four-pointed star, centred around 0,0.
+	 *
+	 * This shape is not convex.
+	 */
+	std::vector<Point2> star;
+
+	/*!
+	 * A triangle (which is always convex).
+	 */
+	std::vector<Point2> triangle;
+
+	/*!
+	 * Prepare to run a test. This creates the fixture members.
+	 */
+	void SetUp() {
+		star.assign({
+			Point2(100.0, 0.0),
+			Point2(20.0, 20.0),
+			Point2(0.0, 100.0),
+			Point2(-20.0, 20.0),
+			Point2(-100.0, 0.0),
+			Point2(-20.0, -20.0),
+			Point2(0.0, -100.0),
+			Point2(20.0, -20.0)
+		});
+		triangle.assign({
+			Point2(0.0, 0.0),
+			Point2(50.0, 0.0),
+			Point2(25.0, 50.0)
+		});
+	}
+};
 
 /*!
  * Test constructing a convex polygon from a predefined set of vertices.
@@ -21,19 +61,17 @@ TEST(ConvexPolygon, ConstructEmpty) {
 /*!
  * Tests constructing a convex polygon with a basic set of vertices.
  */
-TEST(ConvexPolygon, Construct) {
-	std::vector<Point2> vertices = {Point2(10.1, 20.2), Point2(-40.4, 30.3), Point2(0, 20.2)};
-	const ConvexPolygon poly(vertices);
-	EXPECT_EQ(poly.get_vertices(), vertices) << "The vertices in the convex polygon must be the same as the input.";
+TEST_F(ConvexPolygonFixture, Construct) {
+	const ConvexPolygon poly(star);
+	EXPECT_EQ(poly.get_vertices(), star) << "The vertices in the convex polygon must be the same as the input.";
 }
 
 /*!
  * Tests the copy constructor.
  */
-TEST(ConvexPolygon, ConstructCopy) {
-	std::vector<Point2> vertices = {Point2(10.1, 20.2), Point2(-40.4, 30.3), Point2(0, 20.2)};
-	const ConvexPolygon original(vertices);
-	const ConvexPolygon copy(original);
+TEST_F(ConvexPolygonFixture, ConstructCopy) {
+	const ConvexPolygon original(star);
+	const ConvexPolygon copy(star);
 	EXPECT_EQ(original.get_vertices(), copy.get_vertices()) << "The copy must be an exact copy.";
 }
 
