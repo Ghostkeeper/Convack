0ebc9779051eb8f2093a882afbc8488b127b38d8
Ghostkeeper <rubend@tutanota.com>
2020-06-20 12:27:42 +0200

    Add data structure for 2D points
    This doesn't use the PIMPL idiom since I don't expect the interface to change at all for this struct unless the coordinate type is changed in the future.
    The PIMPL idiom would incur unnecessary indirection because it has to dereference that pointer for every access of its memory or any function. That would kill performance here.

---
+++ include/point2.hpp
@@ -0,0 +1,38 @@
+//Library to pack convex polygons into arbitrary shapes.
+//Any copyright is dedicated to the public domain. See LICENSE.md for more details.
+
+#ifndef CONVACK_POINT2
+#define CONVACK_POINT2
+
+#include "coordinate.hpp" //To store the coordinates of this point.
+
+namespace convack {
+
+/*!
+ * This data structure represents a point in 2D space.
+ *
+ * The first axis of this 2D space is termed "X", and the second axis is termed
+ * "Y", by convention.
+ */
+struct Point2 {
+	/*!
+	 * Constructs a new 2D point located at the given coordinates.
+	 * \param x The coordinate of this point on the X axis.
+	 * \param y The coordinate of this point on the Y axis.
+	 */
+	Point2(const coordinate_t x, const coordinate_t y);
+
+	/*!
+	 * The coordinate of this point on the X axis.
+	 */
+	coordinate_t x;
+
+	/*!
+	 * The coordinate of this point on the Y axis.
+	 */
+	coordinate_t y;
+};
+
+}
+
+#endif
---
+++ src/point2.cpp
@@ -0,0 +1,10 @@
+//Library to pack convex polygons into arbitrary shapes.
+//Any copyright is dedicated to the public domain. See LICENSE.md for more details.
+
+#include "point2.hpp"
+
+namespace convack {
+
+Point2::Point2(const coordinate_t x, const coordinate_t y) : x(x), y(y) {}
+
+}
