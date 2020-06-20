21792b2f96159c020953405a1cfc3cae2a36775d
Ghostkeeper <rubend@tutanota.com>
2020-06-20 12:19:28 +0200

    Rename headers to .hpp
    My preference, since it's more consistent with the naming of the .cpp implementation files. I'm not mixing C and C++.

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,7 +1,7 @@
 //Library to pack convex polygons into arbitrary shapes.
 //Any copyright is dedicated to the public domain. See LICENSE.md for more details.
 
-#include "scene.h"
+#include "scene.hpp" //The definitions of the implementation defined here.
 
 namespace convack {
 
