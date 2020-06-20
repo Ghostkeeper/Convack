47b9cea287e8cbda3b58e452b7d59f7c9d38bc12
Ghostkeeper <rubend@tutanota.com>
2020-06-20 12:20:11 +0200

    Add typedef for coordinates
    We'll be using floating points for now. We may switch to doubles at some point if this is not precise enough.

---
+++ include/coordinate.hpp
@@ -0,0 +1,13 @@
+//Library to pack convex polygons into arbitrary shapes.
+//Any copyright is dedicated to the public domain. See LICENSE.md for more details.
+
+#ifndef CONVACK_COORDINATE
+#define CONVACK_COORDINATE
+
+namespace convack {
+
+typedef float coordinate_t;
+
+}
+
+#endif
