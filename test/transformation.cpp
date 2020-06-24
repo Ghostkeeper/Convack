1f479e2d51790967c5b67b5bf072b15fbfed6357
Ghostkeeper <rubend@tutanota.com>
2020-06-24 16:01:09 +0200

    Add test for translating (moving) a point in both X and Y

--- test/transformation.cpp
+++ test/transformation.cpp
@@ -15,7 +15,17 @@ namespace convack {
  */
 TEST(Transformation, Identity) {
 	Transformation identity;
+	ASSERT_EQ(identity.apply(Point2(0, 0)), Point2(0, 0)) << "The identity transformation should not modify the point.";
 	ASSERT_EQ(identity.apply(Point2(13, 37)), Point2(13, 37)) << "The identity transformation should not modify the point.";
+}
+
+/*!
+ * Test translating a point.
+ */
+TEST(Transformation, Translation) {
+	Transformation translation = Transformation().translate(5, -2);
+	ASSERT_EQ(translation.apply(Point2(0, 0)), Point2(5, -2)) << "The translation should move the point +5 in X, and -2 in Y.";
+	ASSERT_EQ(translation.apply(Point2(8, 39)), Point2(13, 37)) << "The translation should move the point +5 in X, and -2 in Y.";
 }
 
 }
