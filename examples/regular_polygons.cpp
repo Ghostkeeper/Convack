ec7f21340a5913dc964c24bee791d9cb99df6041
Ghostkeeper <rubend@tutanota.com>
2020-07-04 12:58:47 +0200

    Add structure for building examples on how to use the library
    These examples will also help to manually test the library...

---
+++ examples/regular_polygons.cpp
@@ -0,0 +1,16 @@
+/*
+ * Library to pack convex polygons into arbitrary shapes.
+ * Any copyright is dedicated to the public domain. See LICENSE.md for more details.
+ */
+
+/* This is a typical example of how to pack a number of convex polygons in a
+scene and get the results. This example involves the following steps:
+- Prepare a number of convex polygons to pack. In this case we're going to pack
+  the regular polygons between 3 and 10 vertices.
+- Instruct Convack to pack these polygons tightly around the 0,0 coordinate.
+- Read out the resulting convex polygons.
+- Write the result to an SVG file. */
+
+int main(int argc, char** argv) {
+    return 0;
+}
