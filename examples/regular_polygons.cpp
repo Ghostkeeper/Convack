6dd112a24b56ec35c05b8e385dea78e6b6c12be5
Ghostkeeper <rubend@tutanota.com>
2020-07-05 21:05:10 +0200

    Add a function to generate regular polygons

--- examples/regular_polygons.cpp
+++ examples/regular_polygons.cpp
@@ -11,6 +11,28 @@ scene and get the results. This example involves the following steps:
 - Read out the resulting convex polygons.
 - Write the result to an SVG file. */
 
+#include <cmath> //To calculate the positions of the vertices for the regular polygons.
+#include <convack/convex_polygon.hpp> //The convex polygons we're going to pack.
+#include <convack/point2.hpp> //To generate convex polygons.
+#include <vector> //To create a list of regular polygons.
+
+/*!
+ * Generate the regular polygons that we're going to be packing.
+ */
+std::vector<convack::ConvexPolygon> create_regular_polygons() {
+	std::vector<convack::ConvexPolygon> result;
+	constexpr float radius = 10;
+	constexpr double pi = std::acos(-1);
+	for(size_t num_sides = 3; num_sides <= 10; ++num_sides) {
+		std::vector<convack::Point2> vertices;
+		for(size_t i = 0; i < num_sides; ++i) {
+			double angle = 2.0 * pi / num_sides * i;
+			vertices.emplace_back(std::cos(angle) * radius, std::sin(angle) * radius);
+		}
+	}
+	return result;
+}
+
 int main(int argc, char** argv) {
-    return 0;
+    std::vector<convack::ConvexPolygon> regular_polygons = create_regular_polygons();
 }
